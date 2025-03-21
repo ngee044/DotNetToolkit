13808153fab4fc919fc78b9b96af9fd0fb8d285f
ngee044 <mgee044@naver.com>
2025-03-21 14:49:25 +0900

    u: Sample 추가

--- Samples/RedisSample/Program.cs
+++ Samples/RedisSample/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Redis.RedisClient;
+
+namespace RedisSample
+{
+	public static class program
+	{
+		public static void Main(string[] args)
+		{
+			var client = new RedisClient(
+				connection_string: "localhost:6379",
+				use_aws_ecs: false,
+				tls_options: null
+			);
+
+			bool connected = client.Connect();
+			if (!connected)
+			{
+				Console.WriteLine("Failed to connect to Redis");
+				return;
+			}
+
+			client.SetString("test", "Hello, Redis!");
+
+			var value = client.GetString("test");
+			Console.WriteLine($"Value: {value}");
+
+			client.Dispose();
+			Console.WriteLine("Done");
+		}
+	}
+}
